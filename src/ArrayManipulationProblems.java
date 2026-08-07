c54dd2b8583c6397c412f99ab827c421c47960e1
Divyansh Shukla <shukladivyansh7392@gmail.com>
2026-08-07 22:43:23 +0530

    ArrayManipulationProblems Revion

--- src/ArrayManipulationProblems.java
+++ src/ArrayManipulationProblems.java
@@ -8,33 +8,38 @@ public class ArrayManipulationProblems {
         int j = n - 1;
 
         while (i <= j) {
-            //swap
+            //swap karna
             int tem = arr[i];
             arr[i] = arr[j];
             arr[j] = arr[tem];
-            //i ko aage badhaao
+
+            //i ko aage badhao
             i++;
-            //j kopiche laao
+            //j ko peeche laao
             j--;
+
         }
-        //now you array has been reversed
+        //now you array have been reversed
         //print
         for (int k : arr) {
             System.out.println(k);
         }
+
+
     }
 
     //2 shift array Elements By one position
     static void shiftBy1(int[] arr) {
         //step1: store last wale ki value
-        int n = arr.length;
-        int temp = arr[n - 1];
+       int n = arr.length;
+       int temp = arr[n-1];
         //Step2: shift all value of array
-        for (int i = n - 1; i > 0; i--) {
-            arr[i] = arr[i - 1];
-        }
+       for(int i=n-1; i<=0; i++){
+           arr[i] = arr[i-1];
+       }
         //step3: temp ki value ko 0 index par copy
         arr[0] = temp;
+
     }
 
     static void printAlternate(int arr[]) {
@@ -70,17 +75,18 @@ public class ArrayManipulationProblems {
     }
 
     static void main() {
-        int arr[] = {1,2,2,3,3,3,4,4,5,5,5,5,5};
-        getMode(arr);
+//        int arr[] = {1,2,2,3,3,3,4,4,5,5,5,5,5};
+//        getMode(arr);
 //        int arr[] = {1,2,3,4,5};
 //        printAlternate(arr);
-    }
+        //}
 //        int arr[] = {1, 2, 3, 4, 5};
-////        reversArray(arr);
+//        reversArray(arr);
 //        shiftBy1(arr);
 //        for (int a : arr) {
 //            System.out.print(a + " ");
 //        }
 //        System.out.println();
 //    }
+    }
 }
